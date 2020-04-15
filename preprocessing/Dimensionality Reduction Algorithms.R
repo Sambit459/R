be95809e609053634ff94ca2c57d5b508e86fbba
Sambit459 <63747131+Sambit459@users.noreply.github.com>
2020-04-16 00:35:14 +0530

    Update Dimensionality Reduction Algorithms.R

--- preprocessing/Dimensionality Reduction Algorithms.R
+++ preprocessing/Dimensionality Reduction Algorithms.R
@@ -1,4 +1,4 @@
 library(stats)
 pca <- princomp(train, cor = TRUE)
-train_reduced  <- predict(pca,train)
-test_reduced  <- predict(pca,test)
+train_reduced <- predict(pca,train)
+test_reduced <- predict(pca,test)
